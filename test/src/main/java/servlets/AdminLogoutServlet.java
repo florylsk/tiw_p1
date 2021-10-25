6aad3713b2c67da4fb5d5a38ede10ba39f0c7bc7
florylsk <florilsk@yahoo.es>
2021-10-25 07:44:39 +0200

    Fix performance

--- test/src/main/java/servlets/AdminLogoutServlet.java
+++ test/src/main/java/servlets/AdminLogoutServlet.java
@@ -2,21 +2,10 @@ package servlets;
 
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,12 +17,9 @@ import javax.jms.QueueSender;
 import javax.jms.QueueSession;
 import javax.jms.Session;
 import javax.jms.TextMessage;
-import javax.naming.Context;
 import javax.naming.InitialContext;
-import javax.sql.DataSource;
 
-import beans.Admin;
-import sql.IAdminConstants;
+
 
 
 @WebServlet(urlPatterns = {"/adminlogout" })
@@ -54,11 +40,13 @@ public class AdminLogoutServlet extends HttpServlet{
 	     QueueConnection con=f.createQueueConnection();  
 	     con.start(); 
         //2) create queue session  
-        QueueSession ses=con.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);  
+        QueueSession ses=con.createQueueSession(false, Session.DUPS_OK_ACKNOWLEDGE);  
         //3) get the Queue object  
         Queue t=(Queue)ctx.lookup("myQueue");  
         //4)create QueueSender object         
         QueueSender sender=ses.createSender(t);  
+        sender.setDisableMessageID(false);
+        sender.setDisableMessageTimestamp(false);
         //5) create TextMessage object  
         TextMessage msg=ses.createTextMessage(); 
 		//6) write message 
@@ -74,6 +62,7 @@ public class AdminLogoutServlet extends HttpServlet{
         //7) send message  
         sender.send(msg);  
         //8) connection close  
+        ses.close();
         con.close();
         session.invalidate();
 		}catch
